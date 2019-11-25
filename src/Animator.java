935c3001bed9e3a2c7ad4faf0cb7c4711978082b
ShafigullinIK <shafigullin.ik@gmail.com>
2019-11-25 12:51:48 +0300

    class Animator - добавлены методы init(), drawFragmentation() и drawToScreen()

--- src/Animator.java
+++ src/Animator.java
@@ -6,19 +6,48 @@ public class Animator {
     private Graphics graphics;
     private Graphics screenGraphics;
     private BufferedImage img;
+    final int STEP = 50;
+    final int WIDTH = 1200;
+    final int HEIGHT = 750;
+    final int POINT_SIZE = 8;
+
+    private Point[][] points = new Point[WIDTH / STEP][HEIGHT / STEP];
 
     public Animator(Graphics g){
         screenGraphics = g;
-        img = new BufferedImage(1200, 750, BufferedImage.TYPE_3BYTE_BGR);
+        img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_3BYTE_BGR);
         graphics = img.getGraphics();
+        init();
+    }
+
+    public void init(){
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0,0, WIDTH, HEIGHT);
+        graphics.setColor(Color.BLACK);
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < points[i].length; j++) {
+                points[i][j] = new Point(STEP * i, STEP * j);
+            }
+        }
+        drawFragmentation();
     }
 
     public void drawFragmentation(){
+        graphics.setColor(Color.BLACK);
 
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < points[i].length; j++) {
+                Point p = points[i][j];
+                graphics.fillOval(p.getX() - POINT_SIZE/2,
+                        p.getY() - POINT_SIZE/2,
+                        POINT_SIZE,
+                        POINT_SIZE);
+            }
+        }
     }
 
     public void drawToScreen(){
-        screenGraphics.drawImage(img, 0, 0, null);
+        screenGraphics.drawImage(img, 0, 0, WIDTH, HEIGHT, null);
     }
 
 }
