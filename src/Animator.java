df18852cbc1eac77016a1a0e9f4676b9014cb8f0
ShafigullinIK <shafigullin.ik@gmail.com>
2019-11-25 12:30:28 +0300

    Animator created

--- src/Animator.java
+++ src/Animator.java
@@ -1,2 +1,24 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class Animator {
+
+    private Graphics graphics;
+    private Graphics screenGraphics;
+    private BufferedImage img;
+
+    public Animator(Graphics g){
+        screenGraphics = g;
+        img = new BufferedImage(1200, 750, BufferedImage.TYPE_3BYTE_BGR);
+        graphics = img.getGraphics();
+    }
+
+    public void drawFragmentation(){
+
+    }
+
+    public void drawToScreen(){
+        screenGraphics.drawImage(img, 0, 0, null);
+    }
+
 }
