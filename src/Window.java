9658ed2edc36825efec57d5ab06709bff71e2a17
ShafigullinIK <shafigullin.ik@gmail.com>
2019-11-25 12:18:03 +0300

    Window created

--- src/Window.java
+++ src/Window.java
@@ -1,6 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame {
 
-
+    public Window(){
+        this.setBounds(100, 100, 1200, 800);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        JPanel panel = new JPanel();
+        panel.setBounds(0,0, 1200, 750);
+        panel.setBackground(Color.WHITE);
+        this.add(panel);
+        this.setVisible(true);
+    }
 }
